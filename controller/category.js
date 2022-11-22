7e6fe29350c7481c3aa6f9bb9792d129b488a6d9
16ramola <kripal.ramola@cynoteck.com>
2022-11-22 15:27:55 +0530

    update description field

--- controller/category.js
+++ controller/category.js
@@ -11,7 +11,7 @@ exports.createCategory = async (req, res) => {
     }
   }
   if(categoryAllReadyExist){
-    return res.send({message:"All ready exist"})
+    return res.send({errorMessage:"All ready exist"})
   }
     try {
       let saveUser = await data.save();
@@ -35,6 +35,7 @@ exports.updateCategory = async(req, res) => {
         const { _id, categoryName, categoryDescription } = req.body
         let user = await category.findById(_id)
         user.categoryName = categoryName
+        user.categoryDescription = categoryDescription
         try {
             let updateEntry = await user.save()
             res.json({ message: "Category updated successfully" })
